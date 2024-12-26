d292f64bb624f8329e5403173d35d1e6403881bb
Nasir Iqbal <140198156+nasiriqbal007@users.noreply.github.com>
2024-12-26 19:09:13 +0500

    Update constants.dart

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,4 +1,4 @@
-const apiKey = 'f134851d56d82452b43cfd4bf1709481';
+const apiKey = 'Add Api key here';
 const imagePath = "https://image.tmdb.org/t/p/w500";
 const actorImagePath = "https://image.tmdb.org/t/p/w200";
 const String baseUrl = 'https://api.themoviedb.org/3';
